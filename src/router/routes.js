d1ecdffc0d399b14cba229ca9ac77dad17b0a336
ZOODUDDLDI <wndud238561@gmail.com>
2024-04-17 21:27:55 +0900

    [delete] admin path. router/idex.js로 해줌.

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,17 +4,18 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
-  {
-    path: '/admin',
-    component: () => import('layouts/admin.vue'), // 관리자용 레이아웃
-    children: [
-      { path: '', component: () => import('adminpages/index.vue') },
-      {
-        path: 'adminCreate',
-        component: () => import('adminpages/adminCreate.vue'),
-      },
-    ],
-  },
+  // TODO
+  // {
+  //   path: '/admin',
+  //   component: () => import('layouts/admin.vue'), // 관리자용 레이아웃
+  //   children: [
+  //     { path: '', component: () => import('adminpages/index.vue') },
+  //     {
+  //       path: 'adminCreate',
+  //       component: () => import('adminpages/adminCreate.vue'),
+  //     },
+  //   ],
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
