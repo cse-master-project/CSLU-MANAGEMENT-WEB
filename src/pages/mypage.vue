b77328f28d896c7f2576748a85efedbfc0e5951a
ZOODUDDLDI <wndud238561@gmail.com>
2024-04-17 21:22:28 +0900

    [create] mypage.vue fix페이지 생성.

---
+++ src/pages/mypage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Mypage</p>
+    <p><router-link to="/mypage/profile">Profile</router-link></p>
+    <router-view />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
