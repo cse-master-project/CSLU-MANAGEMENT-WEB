fdbc085129b31f1df21d0255bfb3da80e0b9b936
ZOODUDDLDI <wndud238561@gmail.com>
2024-04-17 21:14:34 +0900

    [create] 퀴즈 상세 만들기.

---
+++ src/pages/quizzes/[id]/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="text-h4">문제 목록 상세</div>
+    <ul>
+      <li>
+        <router-link to="/mangement">목록보기</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
