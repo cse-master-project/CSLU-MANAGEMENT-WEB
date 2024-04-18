19777e3a7af62712119f33cb7f69b17ba441f084
ZOODUDDLDI <wndud238561@gmail.com>
2024-04-18 14:22:12 +0900

    [update] 메인화면, 서비스소개 페이지

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,7 +1,71 @@
 <template>
-  <div>페이지 소개</div>
+  <q-page class="q-page-custom">
+    <div class="column justify-start">
+      <div class="q-mb-md">
+        <q-card class="card-custom" flat>
+          <div class="centered-img-container">
+            <!-- <q-img src="/public/creative_ideas.jpg" class="q-img-fixed-size" /> -->
+          </div>
+          <q-card-section>
+            <div class="text-h6">창의력을 발휘하고, 지식을 나누어 보세요.</div>
+          </q-card-section>
+          <q-separator />
+          <q-card-section>
+            <p>
+              당신만의 독창적인 문제를 만들어 전 세계 사람들과 지식을 공유할 수
+              있는 기회입니다. 당신의 아이디어가 새로운 학습의 씨앗이 됩니다.
+            </p>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div>
+        <q-card class="card-custom" flat>
+          <div class="centered-img-container">
+            <!-- <q-img src="/public/global_learning.jpg" class="q-img-fixed-size" /> -->
+          </div>
+          <q-card-section>
+            <div class="text-h6">당신의 지식이 전 세계의 학습을 이끕니다.</div>
+          </q-card-section>
+          <q-separator />
+          <q-card-section>
+            <p>
+              웹 플랫폼에서 직접 문제를 제작하고, 우리의 전문가 팀이 이를
+              검토합니다. 승인된 문제들은 앱을 통해 전 세계 수많은 사용자들에게
+              도달, 모두가 당신의 지식으로부터 배울 수 있습니다.
+            </p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style>
+.q-page-custom {
+  padding: 16px;
+}
+
+.card-custom {
+  height: 90vh;
+  overflow-y: auto;
+  scrollbar-width: none; /* 파이어폭스에서 스크롤바를 숨김 */
+}
+
+.card-custom::-webkit-scrollbar {
+  display: none; /* 웹킷 기반 브라우저에서 스크롤바를 숨김 */
+}
+
+.q-img-fixed-size {
+  max-width: 100%;
+  max-height: 300px;
+  object-fit: cover;
+}
+
+.centered-img-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>
-    <div>home</div>
-  </div>
+  <q-page class="q-pa-md flex flex-center">
+    <div class="row justify-center q-col-gutter-md" style="max-width: 1080px">
+      <div class="col-12 col-md-8">
+        <q-card flat class="q-pa-md text-center">
+          <q-card-section>
+            <div class="text-h5">경상국립대 컴공 신입생의 첫걸음</div>
+            <div class="text-subtitle1">
+              복습에서부터 성공까지, 당신을 안내합니다.
+            </div>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="q-pt-none">
+            <p>
+              신입생 여러분을 위한 최적의 복습 가이드를 제공합니다. 여러분의
+              컴퓨터 공학 여정을 지원하며, 성공적인 학습 결과를 위한 모든 자료를
+              한눈에 확인할 수 있습니다.
+            </p>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-12 col-md-4">
+        <img src="/gnu.jpeg" class="full-width" alt="경상국립대 이미지" />
+      </div>
+    </div>
+  </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+// 필요한 스크립트를 여기에 추가
+</script>
 
-<style lang="scss" scoped></style>
+<style>
+.full-width {
+  width: 100%;
+  height: auto;
+}
+
+/* 반응형 디자인을 위한 추가 스타일 */
+@media (max-width: 768px) {
+  .q-card {
+    margin-bottom: 20px;
+  }
+}
+</style>
