47c3b856beaf7e4bc6480e6b6e2aa32aa814bf33
ZOODUDDLDI <wndud238561@gmail.com>
2024-04-27 19:40:23 +0900

    [update] 메인화면, 서비스소개 페이지 레이이아웃배치 변경.

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,40 +1,28 @@
 <template>
   <q-page class="q-page-custom">
-    <div class="column justify-start">
+    <div class="column justify-center">
       <div class="q-mb-md">
         <q-card class="card-custom" flat>
-          <div class="centered-img-container">
-            <!-- <q-img src="/public/creative_ideas.jpg" class="q-img-fixed-size" /> -->
-          </div>
           <q-card-section>
-            <div class="text-h6">창의력을 발휘하고, 지식을 나누어 보세요.</div>
+            <div class="text-h5">창의력을 발휘하고, 지식을 나누어 보세요.</div>
           </q-card-section>
           <q-separator />
-          <q-card-section>
+          <q-card-section style="font-size: 15px">
             <p>
-              당신만의 독창적인 문제를 만들어 전 세계 사람들과 지식을 공유할 수
-              있는 기회입니다. 당신의 아이디어가 새로운 학습의 씨앗이 됩니다.
+              우리의 서비스는 학습자들이 자신만의 독창적인 문제를 만들고 공유할
+              수 있는 플랫폼입니다. 여러분은 이 서비스를 통해 자신의 창의력을
+              발휘하고, 다른 이들과 지식을 나눌 수 있습니다.
+            </p>
+            <p>
+              문제 제작 서비스를 통해 여러분은 자신의 학습 경험을 향상시키고,
+              다른 이들의 학습에 기여할 수 있습니다. 또한, 다양한 문제를 풀며
+              자신의 이해도를 확인하고, 커뮤니티와 지식을 공유함으로써 자신의
+              전문성을 향상시킬 수 있습니다.
             </p>
           </q-card-section>
-        </q-card>
-      </div>
-
-      <div>
-        <q-card class="card-custom" flat>
           <div class="centered-img-container">
-            <!-- <q-img src="/public/global_learning.jpg" class="q-img-fixed-size" /> -->
+            <q-img src="/public/creative_ideas.jpg" class="q-img-fixed-size" />
           </div>
-          <q-card-section>
-            <div class="text-h6">당신의 지식이 전 세계의 학습을 이끕니다.</div>
-          </q-card-section>
-          <q-separator />
-          <q-card-section>
-            <p>
-              웹 플랫폼에서 직접 문제를 제작하고, 우리의 전문가 팀이 이를
-              검토합니다. 승인된 문제들은 앱을 통해 전 세계 수많은 사용자들에게
-              도달, 모두가 당신의 지식으로부터 배울 수 있습니다.
-            </p>
-          </q-card-section>
         </q-card>
       </div>
     </div>
@@ -51,6 +39,7 @@
 .card-custom {
   height: 90vh;
   overflow-y: auto;
+
   scrollbar-width: none; /* 파이어폭스에서 스크롤바를 숨김 */
 }
 
@@ -60,7 +49,7 @@
 
 .q-img-fixed-size {
   max-width: 100%;
-  max-height: 300px;
+  max-height: 500px;
   object-fit: cover;
 }
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,29 +1,55 @@
 <template>
-  <q-page class="q-pa-md flex flex-center">
+  <q-page class="q-pa-md flex flex-center" style="min-height: 700px">
     <div class="row justify-center q-col-gutter-md" style="max-width: 1080px">
-      <div class="col-12 col-md-8">
-        <q-card flat class="q-pa-md text-center">
+      <div class="col-6 col-md-8">
+        <q-card flat class="q-pa-md text-left">
           <q-card-section>
-            <div class="text-h5">경상국립대 컴공 신입생의 첫걸음</div>
-            <div class="text-subtitle1">
+            <div class="text-h4">경상국립대 컴공 신입생의 첫걸음</div>
+            <div class="text-subtitle1" style="margin: 20px 0">
               복습에서부터 성공까지, 당신을 안내합니다.
             </div>
           </q-card-section>
           <q-separator />
-          <q-card-section class="q-pt-none">
+          <q-card-section class="q-pt-none" style="margin: 20px 0">
             <p>
               신입생 여러분을 위한 최적의 복습 가이드를 제공합니다. 여러분의
               컴퓨터 공학 여정을 지원하며, 성공적인 학습 결과를 위한 모든 자료를
               한눈에 확인할 수 있습니다.
             </p>
+            <p>
+              저희의 플랫폼에서는 다양한 학습 자료를 제공하며, 여러분이 공부를
+              시작하고 발전할 수 있도록 돕습니다. 필요한 정보와 도구들을 쉽게
+              찾을 수 있으며, 성공적인 학습을 위한 리소스들을 제공합니다.
+              함께하여 여러분의 컴퓨터 공학 학습을 성공적으로 이끌어 나가는 데
+              도움이 되는 것이 우리의 목표입니다. 함께 여행을 시작해 봅시다!
+            </p>
           </q-card-section>
         </q-card>
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-6 col-md-4">
         <img src="/gnu.jpeg" class="full-width" alt="경상국립대 이미지" />
       </div>
     </div>
   </q-page>
+  <div class="down">
+    <a href="#" class="link-wrapper">
+      <div class="img-wrapper">
+        <img src="/apple-logo.png" />
+      </div>
+      <div class="content-wrapper">
+        <h6>App Store</h6>
+      </div>
+    </a>
+
+    <a href="#" class="link-wrapper">
+      <div class="img-wrapper">
+        <img src="/google-play.png" />
+      </div>
+      <div class="content-wrapper">
+        <h6>Google Play</h6>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script setup>
@@ -31,11 +57,60 @@
 </script>
 
 <style>
+.text-subtitle1 {
+  font-size: 20px;
+}
+.q-pt-none {
+  font-size: 16px;
+}
 .full-width {
   width: 100%;
   height: auto;
 }
+.down {
+  display: flex;
+}
+.img-wrapper {
+  width: 48px;
+  display: flex;
+  align-items: center;
+}
 
+img {
+  width: 80%;
+}
+
+.link-wrapper {
+  background: #111;
+  color: #e1e1e1 !important;
+  width: 200px;
+  height: 60px;
+  text-decoration: none;
+  padding: 28px 16px;
+  border-radius: 4px;
+  font-family: 'Poppins', sans-serif;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 30px;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 400px;
+}
+
+.content-wrapper {
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 18px;
+  flex-direction: column;
+  width: 200px;
+}
 /* 반응형 디자인을 위한 추가 스타일 */
 @media (max-width: 768px) {
   .q-card {
