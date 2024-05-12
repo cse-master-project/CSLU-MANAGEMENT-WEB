586df55bc27802e203e7a3bd5596ccea12743a13
ZOODUDDLDI <wndud238561@gmail.com>
2024-05-12 15:46:16 +0900

    [update] 서버에서 받아온 단답형 뿌려주기 구현.

--- src/components/quiztype/user/UserShortAnswerView.vue
+++ src/components/quiztype/user/UserShortAnswerView.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <div>{{ quizcontent.quiz }}</div>
-  </div>
+  <q-form class="q-pa-md">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6 q-mb-md">문제 유형: 단답형</div>
+        <div class="text-subtitle1">문제 : {{ quizcontent.quiz }}</div>
+      </q-card-section>
+      <q-card-section>
+        <div>정답: {{ quizcontent.answer }}</div>
+      </q-card-section>
+      <q-card-section>
+        <div>해설: {{ quizcontent.commentary }}</div>
+      </q-card-section>
+    </q-card>
+  </q-form>
 </template>
 
 <script setup>
