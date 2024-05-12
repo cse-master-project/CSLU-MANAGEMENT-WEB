1ed8d2b58d955923fd80c6ec50585eb8e9596297
ZOODUDDLDI <wndud238561@gmail.com>
2024-05-12 15:43:25 +0900

    [update] 서버에서 가져오면, 4지선다형 보여주기 구현.

--- src/components/quiztype/user/UserMultipleChoiceView.vue
+++ src/components/quiztype/user/UserMultipleChoiceView.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    <div>{{ quizcontent.quiz }}</div>
-  </div>
+  <q-form class="q-pa-md">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6 q-mb-md">문제 유형: 4지선다형</div>
+        <div class="text-subtitle1">문제 : {{ quizcontent.quiz }}</div>
+      </q-card-section>
+
+      <q-card-section>
+        <div
+          v-for="(option, index) in quizcontent.option"
+          :key="index"
+          class="q-mb-md"
+        >
+          {{ index + 1 }}. {{ option }}
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <div>정답: {{ quizcontent.option[quizcontent.answer - 1] }}</div>
+      </q-card-section>
+
+      <q-card-section>
+        <div>해설: {{ quizcontent.commentary }}</div>
+      </q-card-section>
+    </q-card>
+  </q-form>
 </template>
 
 <script setup>
