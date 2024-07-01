9b7f0b2f3788786f971ad0c2926f47a5e9d662db
ZOODUDDLDI <wndud238561@gmail.com>
2024-07-01 16:11:36 +0900

    [badrequest] 일단 배드 리퀘스트지만 한것까지 커밋.

--- src/components/quiztype/TrueOrFalse.vue
+++ src/components/quiztype/TrueOrFalse.vue
@@ -8,10 +8,11 @@
           label="대분류"
           outlined
           class="q-mb-md"
+          @update:model-value="updateDetailSubjectOptions"
         />
         <q-select
-          v-model="detailSubjet"
-          :options="detailSubjectOptions"
+          v-model="detailSubject"
+          :options="filteredDetailSubjectOptions"
           label="소분류"
           outlined
           class="q-mb-md"
@@ -65,13 +66,21 @@
       </q-card-actions>
     </q-card>
   </q-form>
+  <!-- SubmitQuizSuccess 컴포넌트 -->
+  <SubmitQuizSuccess
+    v-if="submitQuizSuccess"
+    :submit-quiz-success="submitQuizSuccess"
+  />
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue';
-import { QInput, QOptionGroup } from 'quasar';
+import { ref, defineEmits, onMounted } from 'vue';
+import { api } from 'src/boot/axios';
+import SubmitQuizSuccess from 'src/components/quiz/SubmitQuizSuccess.vue';
+import useCategories from 'src/services/useCategories.js';
 
 const emits = defineEmits(['change-quiz-type']);
+
 const goBack = () => {
   emits('change-quiz-type', '');
 };
@@ -84,40 +93,73 @@ const fileInputHandler = event => {
   }
 };
 
-const subjectOptions = [
-  { label: 'c언어', value: 'C' },
-  { label: '파이썬', value: 'Python' },
-  { label: '자료구조', value: 'Data structure' },
-];
+const { categories, subjectOptions, detailSubjectOptions, fetchCategories } =
+  useCategories();
 
-const detailSubjectOptions = [
-  { label: '스택', value: 'Stack' },
-  { label: '큐', value: 'Queue' },
-  { label: '그래프', value: 'Graph' },
-];
+onMounted(fetchCategories);
+
 const options = [
-  { label: 'O', value: '1' },
-  { label: 'X', value: '0' },
+  { label: 'O', value: 1 },
+  { label: 'X', value: 0 },
 ];
-
 const subject = ref('');
-const detailSubjet = ref('');
+const detailSubject = ref('');
 const quiz = ref('');
 const selectedAnswer = ref(null);
 const commentary = ref('');
 
+// 대분류 선택에 따라 소분류 옵션을 업데이트하는 함수
+const updateDetailSubjectOptions = () => {
+  const selectedCategory = categories.value.find(
+    category => category.subject === subject.value,
+  );
+  if (selectedCategory) {
+    filteredDetailSubjectOptions.value = selectedCategory.detailSubject;
+  } else {
+    filteredDetailSubjectOptions.value = [];
+  }
+};
+
+const filteredDetailSubjectOptions = ref([]);
+
+const submitQuizSuccess = ref(false);
+
 const submitQuiz = () => {
   const quizData = {
     subjectId: subject.value,
-    detailSubject: detailSubjet.value,
+    detailSubject: detailSubject.value,
+    quizType: '4',
     jsonContent: JSON.stringify({
-      type: '4',
       quiz: quiz.value,
       answer: selectedAnswer.value,
       commentary: commentary.value,
     }),
   };
   console.log('서버에 제출될 데이터:', quizData);
+  api
+    .post('/api/quiz/default', quizData)
+    .then(response => {
+      // console.log('서버 응답:', response.data);
+      submitQuizSuccess.value = true;
+    })
+    .catch(error => {
+      //console.error('서버 응답 오류:', error);
+      if (error.response.status === 400) {
+        // 예: 사용자에게 문제가 부족하거나 잘못된 데이터를 입력했다고 알림
+        alert(
+          '입력된 데이터가 부족하거나 잘못되었습니다. 빈칸이 없는지 확인해주세요 ^_^',
+        );
+      } else if (error.response.status === 500) {
+        // 예: 서버 측에서 처리 중 오류 발생
+        alert(
+          '서버에서 문제를 처리하는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+        );
+      } else {
+        // 기타 다른 오류 상황에 대한 처리
+        alert('문제 등록 중 예상치 못한 오류가 발생했습니다.');
+      }
+      // 실패 시 사용자 경험을 개선할 수 있는 추가적인 로직 추가
+    });
 };
 </script>
 
