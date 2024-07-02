6c38bc5be4b13437d7865ca7657779cf1ff404ac
ZOODUDDLDI <wndud238561@gmail.com>
2024-07-02 09:33:36 +0900

    [update] 서버전송에 맞게 빈칸채우기 문제 완성.

--- src/components/quiztype/FillInTheBlank.vue
+++ src/components/quiztype/FillInTheBlank.vue
@@ -2,20 +2,24 @@
   <q-form class="q-pa-md">
     <q-card>
       <q-card-section>
+        <!-- 대분류 선택 -->
         <q-select
           v-model="subject"
           :options="subjectOptions"
           label="대분류"
           outlined
           class="q-mb-md"
+          @update:model-value="updateDetailSubjectOptions"
         />
+        <!-- 소분류 선택 -->
         <q-select
-          v-model="detailSubjet"
-          :options="detailSubjectOptions"
+          v-model="detailSubject"
+          :options="filteredDetailSubjectOptions"
           label="소분류"
           outlined
           class="q-mb-md"
         />
+        <!-- 문제 입력 -->
         <div>
           <q-input
             v-model="quiz"
@@ -31,6 +35,7 @@
           </q-tooltip>
         </div>
 
+        <!-- 답변 입력 -->
         <div v-for="(answer, index) in answers" :key="index" class="q-mb-md">
           <q-input
             v-model="answers[index]"
@@ -42,6 +47,7 @@
           />
         </div>
 
+        <!-- 해설 입력 -->
         <q-input
           v-model="commentary"
           type="textarea"
@@ -52,7 +58,7 @@
           style="margin: 3% 0"
         />
 
-        <!--첨부파일-->
+        <!-- 파일 첨부 -->
         <section class="container">
           <label for="file">
             <div class="styled-file-input">
@@ -67,26 +73,36 @@
       <q-card-actions align="right">
         <q-btn
           class="backbtn"
-          @click="goBack()"
+          @click="goBack"
           style="width: 10%; margin: 3% 1%"
-          >뒤로가기</q-btn
         >
+          뒤로가기
+        </q-btn>
         <q-btn
           class="registerbtn"
           @click="submitQuiz"
           style="width: 10%; margin: 3% 0"
-          >문제 등록</q-btn
         >
+          문제 등록
+        </q-btn>
       </q-card-actions>
     </q-card>
   </q-form>
+  <!-- SubmitQuizSuccess 컴포넌트 -->
+  <SubmitQuizSuccess
+    v-if="submitQuizSuccess"
+    :submit-quiz-success="submitQuizSuccess"
+  />
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue';
-import { QInput } from 'quasar';
+import { ref, defineEmits, onMounted } from 'vue';
+import { api } from 'src/boot/axios';
+import SubmitQuizSuccess from 'src/components/quiz/SubmitQuizSuccess.vue';
+import useCategories from 'src/services/useCategories.js';
 
 const emits = defineEmits(['change-quiz-type']);
+
 const goBack = () => {
   emits('change-quiz-type', '');
 };
@@ -99,36 +115,62 @@ const fileInputHandler = event => {
   }
 };
 
-const subjectOptions = [
-  { label: 'c언어', value: 'C' },
-  { label: '파이썬', value: 'Python' },
-  { label: '자료구조', value: 'Data structure' },
-];
+const { categories, subjectOptions, detailSubjectOptions, fetchCategories } =
+  useCategories();
 
-const detailSubjectOptions = [
-  { label: '스택', value: 'Stack' },
-  { label: '큐', value: 'Queue' },
-  { label: '그래프', value: 'Graph' },
-];
+onMounted(fetchCategories);
 
 const subject = ref('');
-const detailSubjet = ref('');
+const detailSubject = ref('');
 const quiz = ref('');
 const answers = ref(['']);
 const commentary = ref('');
 
+const updateDetailSubjectOptions = () => {
+  const selectedCategory = categories.value.find(
+    category => category.subject === subject.value,
+  );
+  if (selectedCategory) {
+    filteredDetailSubjectOptions.value = selectedCategory.detailSubject;
+  } else {
+    filteredDetailSubjectOptions.value = [];
+  }
+};
+
+const filteredDetailSubjectOptions = ref([]);
+
+const submitQuizSuccess = ref(false);
+
 const submitQuiz = () => {
   const quizData = {
-    subjectId: subject.value,
-    detailSubject: detailSubjet.value,
+    subject: subject.value,
+    detailSubject: detailSubject.value,
+    quizType: '5',
     jsonContent: JSON.stringify({
-      type: '5',
       quiz: quiz.value,
       answer: answers.value,
       commentary: commentary.value,
     }),
   };
   console.log('서버에 제출될 데이터:', quizData);
+  api
+    .post('/api/quiz/default', quizData)
+    .then(response => {
+      submitQuizSuccess.value = true;
+    })
+    .catch(error => {
+      if (error.response.status === 400) {
+        alert(
+          '입력된 데이터가 부족하거나 잘못되었습니다. 빈칸이 없는지 확인해주세요 ^_^',
+        );
+      } else if (error.response.status === 500) {
+        alert(
+          '서버에서 문제를 처리하는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+        );
+      } else {
+        alert('문제 등록 중 예상치 못한 오류가 발생했습니다.');
+      }
+    });
 };
 </script>
 
