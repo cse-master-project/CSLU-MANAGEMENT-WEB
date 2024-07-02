0c7b5ce47488c902255f3d35e0507b54e4d5dd2f
ZOODUDDLDI <wndud238561@gmail.com>
2024-07-02 13:33:00 +0900

    [update] 관리자 문제 상세조회 리스트에 폼 동적으로 보여주기.

--- src/pages/quizzes/[id]/index.vue
+++ src/pages/quizzes/[id]/index.vue
@@ -64,23 +64,23 @@ const quizTypeViewForm = quizType => {
   switch (quizType) {
     case 1:
       return defineAsyncComponent(() =>
-        import('src/components/quiztype/user/UserMultipleChoiceView.vue'),
+        import('src/components/quiztype/quizView/MultipleChoiceView.vue'),
       );
     case 2:
       return defineAsyncComponent(() =>
-        import('src/components/quiztype/user/UserShortAnswerView.vue'),
+        import('src/components/quiztype/quizView/ShortAnswerView.vue'),
       );
     case 3:
       return defineAsyncComponent(() =>
-        import('src/components/quiztype/user/UserMatchingView.vue'),
+        import('src/components/quiztype/quizView/MatchingView.vue'),
       );
     case 4:
       return defineAsyncComponent(() =>
-        import('src/components/quiztype/user/UserTrueOrFalseView.vue'),
+        import('src/components/quiztype/quizView/TrueOrFalseView.vue'),
       );
     case 5:
       return defineAsyncComponent(() =>
-        import('src/components/quiztype/user/UserFillInTheBlank.vue'),
+        import('src/components/quiztype/quizView/FillInTheBlank.vue'),
       );
     default:
       return null;
