db009e82263d14f16ae5afc73ef189592ecee231
ZOODUDDLDI <wndud238561@gmail.com>
2024-07-09 19:44:45 +0900

    [update] 과목, 챕터 목록 생성.

--- src/pages/quizzes/[id]/components/QuizList.vue
+++ src/pages/quizzes/[id]/components/QuizList.vue
@@ -1,6 +1,20 @@
 <template>
   <q-page padding>
     <div class="q-pa-md">
+      <q-select
+        v-model="subject"
+        :options="subjectOptions"
+        label="과목 선택"
+        class="q-mb-md"
+        @update:model-value="updateDetailSubjectOptions"
+      ></q-select>
+      <q-select
+        v-model="detailSubject"
+        :options="filteredDetailSubjectOptions"
+        label="챕터 선택"
+        class="q-mb-md"
+      ></q-select>
+      <q-btn @click="sortQuizzes">문제정렬</q-btn>
       <q-list bordered class="rounded-borders">
         <div v-for="quiz in quizzes" :key="quiz.quizId" class="q-mb-md">
           <q-card
@@ -11,16 +25,20 @@
           >
             <q-card-section class="card-content">
               <div class="row justify-between">
-                <div class="text-h6 text-subject">{{ quiz.subject }}</div>
+                <div class="text-h6 text-subject">
+                  과목 : {{ quiz.subject }}
+                </div>
                 <div class="text-body2">
                   {{ formatQuizType(quiz.quizType) }}
                 </div>
               </div>
               <div class="row justify-between">
-                <div class="text-subtitle2">{{ quiz.detailSubject }}</div>
+                <div class="text-subtitle2">
+                  챕터 : {{ quiz.detailSubject }}
+                </div>
               </div>
               <div class="text-caption text-createAt">
-                {{ formatDate(quiz.createAt) }}
+                생성일 : {{ formatDate(quiz.createAt) }}
               </div>
             </q-card-section>
           </q-card>
@@ -35,44 +53,68 @@ import { ref, onMounted } from 'vue';
 import { api } from 'src/boot/axios';
 import { useRouter } from 'vue-router';
 import { date } from 'quasar';
+import useCategories from 'src/services/useCategories.js';
+
+//카테고리 조회 서비스 사용.
+const { subjectOptions, fetchCategories, getDetailSubjectsBySubject } =
+  useCategories();
 
 const quizzes = ref([]);
+const subject = ref('');
+const detailSubject = ref('');
+const filteredDetailSubjectOptions = ref([]);
 
+onMounted(async () => {
+  await fetchCategories();
+  await fetchQuizzes();
+});
+
+//서버에서 퀴즈 목록 들고 오기.
 const fetchQuizzes = async () => {
   try {
     const response = await api.get('/api/quiz/default');
     quizzes.value = response.data.content;
-    console.log(quizzes.value);
+    console.log('퀴즈목록 : ', quizzes.value);
   } catch (error) {
     console.error('퀴즈 데이터를 불러오는데 실패했습니다.', error);
   }
 };
 
-onMounted(fetchQuizzes);
-
 const router = useRouter();
 
-function goToQuizDetail(quizId) {
+const goToQuizDetail = quizId => {
   router.push(`/quizzes/${quizId}`);
-}
+};
 
+//대분류에 따른 소분류 필터링 함수.
+const updateDetailSubjectOptions = () => {
+  filteredDetailSubjectOptions.value = getDetailSubjectsBySubject(
+    subject.value,
+  );
+};
+
+//
+const sortQuizzes = () => {};
+
+// 문제 형식에 따라 유형 알려주기.
 const formatQuizType = quizType => {
   switch (quizType) {
     case 1:
-      return '4지선다형';
+      return '<4지선다형>';
     case 2:
-      return '단답형';
+      return '<단답형>';
     case 3:
-      return '선긋기형';
+      return '<선긋기형>';
     case 4:
-      return 'O/X형';
+      return '<O/X형>';
     case 5:
-      return '빈칸 채우기형';
+      return '<빈칸 채우기형>';
     default:
-      return '알 수 없는 유형';
+      return '<알 수 없는 유형>';
   }
 };
 
+// 시간 알려주기.
 const formatDate = dateString => {
   return date.formatDate(dateString, 'YYYY-MM-DD HH:mm:ss');
 };
