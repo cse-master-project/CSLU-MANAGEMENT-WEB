04c7aeb39f84c12e1b7737833ce21ae0165ed7d1
ZOODUDDLDI <104435550+ZOODUDDLDI@users.noreply.github.com>
2024-07-13 15:24:28 +0900

    [update] google login

--- src/components/auth/UserLoginGoogle.vue
+++ src/components/auth/UserLoginGoogle.vue
@@ -9,10 +9,14 @@
         <q-btn flat color="primary" label="구글로그인" @click="LoginGoogle" />
       </q-card-actions>
     </q-card>
+
+    <GoogleLogin :callback="callback" />
   </q-dialog>
 </template>
 
 <script setup>
+import { googleSdkLoaded } from 'vue3-google-login';
+import axios from 'axios';
 import { ref, defineProps } from 'vue';
 
 const props = defineProps({
@@ -23,6 +27,56 @@ const visible = ref(props.isLogin);
 
 const LoginGoogle = () => {
   console.log('구글로그인');
+  googleSdkLoaded(google => {
+    google.accounts.oauth2
+      .initCodeClient({
+        scope: 'email profile openid',
+        redirect_uri: 'http://localhost:9000',
+        callback: response => {
+          if (response.code) {
+            this.sendCodeToBackend(response.code);
+          }
+        },
+      })
+      .requestCode();
+  });
+  sendCodeToBackend = async code => {
+    try {
+      const response = await axios.post('https://oauth2.googleapis.com/token', {
+        code,
+        redirect_uri: 'postmessage',
+        grant_type: 'authorization_code',
+      });
+
+      const accessToken = response.data.access_token;
+      console.log(accessToken);
+
+      // Fetch user details using the access token
+      const userResponse = await axios.get(
+        'https://www.googleapis.com/oauth2/v3/userinfo',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+
+      if (userResponse && userResponse.data) {
+        // Set the userDetails data property to the userResponse object
+        this.userDetails = userResponse.data;
+      } else {
+        // Handle the case where userResponse or userResponse.data is undefined
+        console.error('Failed to fetch user details.');
+      }
+    } catch (error) {
+      console.error('Token exchange failed:', error.response.data);
+    }
+  };
+  const callback = response => {
+    // This callback will be triggered when the user selects or login to
+    // his Google account from the popup
+    console.log('Handle the response', response);
+  };
 };
 </script>
 
