e159a183719ea22e26887bf06eab032624e9bf77
ZOODUDDLDI <wndud238561@gmail.com>
2024-10-02 16:19:05 +0900

    [update] 디자인 변경 (card 크기 outo로)

--- src/components/auth/UserLoginGoogle.vue
+++ src/components/auth/UserLoginGoogle.vue
@@ -51,6 +51,7 @@
             placeholder="닉네임"
             class="q-mb-md"
             maxlength="20"
+            counter
           >
             <q-tooltip
               anchor="top middle"
@@ -227,7 +228,7 @@ async function fetchInfo() {
 }
 
 .my-card {
-  height: 60%;
+  height: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -264,7 +265,7 @@ async function fetchInfo() {
 .signupbtn {
   background-color: rgba(0, 12, 30, 0.8);
   width: 100%;
-  margin: 4% auto;
+  margin: 8% auto;
   font-family: 'Toss Product Sans';
   font-weight: 600;
   color: white;
