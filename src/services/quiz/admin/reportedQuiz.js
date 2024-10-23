851d40a9f3f5f3c58f7af9ad814541ad2b8ba777
ZOODUDDLDI <wndud238561@gmail.com>
2024-10-23 11:13:33 +0900

    [update] api 로직 분리.

--- src/services/quiz/admin/reportedQuiz.js
+++ src/services/quiz/admin/reportedQuiz.js
@@ -16,3 +16,12 @@ export const fetchQuizzesFromApi = async () => {
     throw error; // 호출하는 곳에서 오류를 처리할 수 있도록 예외를 던집니다.
   }
 };
+
+export const fetchReoportsFromApi = async quizId => {
+  try {
+    const response = await api.get(`/api/v2/quiz/${quizId}/report`);
+    return response.data.content;
+  } catch (error) {
+    console.error('퀴즈 데이터를 불러오는데 실패했습니다.', error);
+  }
+};
